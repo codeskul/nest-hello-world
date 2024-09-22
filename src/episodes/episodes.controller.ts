48ebf8d7752a7c7be7ee911e70a52e077ba7f345
sam new lap <sam@gmail.com>
2024-09-22 11:22:26 +0530

    error handeling

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { CreateEpisodeDto } from './dto/create-episode.dto';
 import { ConfigService } from '../config/config.service';
@@ -24,7 +34,8 @@ export class EpisodesController {
   async findOne(@Param('id') id: string) {
     const episode = await this.episodesService.findOne(id);
     if (!episode) {
-      throw new Error('Episode not found');
+      throw new NotFoundException('Episode not found');
+      // throw new HttpException('Episode not found', HttpStatus.NOT_FOUND);
     }
     return episode;
   }
