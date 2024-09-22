76ed43898a09ba2f7bf3e01f1708e60d5913f4bb
sam new lap <sam@gmail.com>
2024-09-22 10:08:29 +0530

    servide provider

--- src/episodes/episodes.service.ts
+++ src/episodes/episodes.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Episode } from './entity/episodes.entity';
+import { CreateEpisodeDto } from './dto/create-episode.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class EpisodesService {}
+export class EpisodesService {
+  private episodes: Episode[] = [];
+
+  async findAll(sort: 'asc' | 'desc' = 'desc') {
+    const sortAsc = (a: Episode, b: Episode) => (a.name > b.name ? 1 : -1);
+    const sortDesc = (a: Episode, b: Episode) => (a.name < b.name ? 1 : -1);
+
+    return sort === 'asc'
+      ? this.episodes.sort(sortAsc)
+      : this.episodes.sort(sortDesc);
+  }
+
+  async findFeatured() {
+    return this.episodes.find((episode) => episode.featured);
+  }
+
+  async findOne(id: string) {
+    return this.episodes.find((episode) => episode.id === id);
+  }
+
+  async create(createEpisodeDto: CreateEpisodeDto) {
+    const newEpisode = { ...createEpisodeDto, id: randomUUID() };
+
+    this.episodes.push(newEpisode);
+    return newEpisode;
+  }
+}
