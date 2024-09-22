f02cd9a823cebfdd3a35d19d3375c5be61c6df59
sam new lap <sam@gmail.com>
2024-09-22 09:58:44 +0530

    modules and controller

---
+++ src/episodes/episodes.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+
+@Controller('episodes')
+export class EpisodesController {
+  @Get()
+  findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
+    console.log(sort);
+    return 'all episodes';
+  }
+
+  @Get('featured')
+  findFeatured() {
+    return 'featured episodes';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `episode with id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() input: any) {
+    return 'create a new episode';
+  }
+}
